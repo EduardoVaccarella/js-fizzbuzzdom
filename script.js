2b54a36eada3ee694ac50acb3072154686e55bf5
eduardoego <eduardo0vaccarella@gmail.com>
2022-02-07 23:32:11 +0100

    Finitoo

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const ul = document.querySelector("ul.list");
 
 for (let x = 1; x < 101; x++) {
-    const element = `<li class="box box--${x}">${x}</li>`;
-    ul.innerHTML += element;
+    if (x % 15 == 0) {
+        const element15 = `<li class="box box15">FizzBuzz</li>`;
+        ul.innerHTML += element15;
+    }
+
+    else if (x % 3 == 0) {
+        const element3 = `<li class="box box3">Fizz</li>`;
+        ul.innerHTML += element3;
+    }
+
+    else if (x % 5 == 0) {
+        const element5 = `<li class="box box5">Buzz</li>`;
+        ul.innerHTML += element5;
+    }
+
+    else {
+        const element = `<li class="box box1">${x}</li>`;
+        ul.innerHTML += element;
+    }
 }
